6f3f8e0c4c3e46d7eb32ceaea7d5c312ae1cee51
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-04 05:49:08 +0200

    Adding Typography-Font CheatSheets

---
+++ Typography-Font/Unicode Code Charts Quick Reference/webscripts/lastModified.js
@@ -0,0 +1,3 @@
+  document.writeln("<font size='-3'>Last updated:&nbsp;");
+  document.writeln("- " + new Date(document.lastModified).toLocaleString());       
+  document.writeln("</font>");       
